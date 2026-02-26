b5b8dbd635070498b9e10514bfe8c6f6f81b4b15
zhanghan <dfhh198902@yahoo.co.jp>
2026-02-26 13:15:50 +0900

    style(label): set barcode margin-top 2mm and padding 0 3mm

--- apps/api/public/app.js
+++ apps/api/public/app.js
@@ -383,8 +383,8 @@ function openPrintLabelWindow(labelData) {
         align-items: flex-start;
         justify-content: center;
         box-sizing: border-box;
-        padding: 0 2mm;
-        margin-top: 1.2mm;
+        padding: 0 3mm;
+        margin-top: 2mm;
         overflow: hidden;
       }
       .label-barcode svg {
